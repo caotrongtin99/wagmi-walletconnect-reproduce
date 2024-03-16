b1724b8b3b4994ec1b268c9eb49c6cea0abaef7c
caotrongtin99 <tin.caotrong@gmail.com>
2024-03-16 20:45:35 +0700

    finish

---
+++ components/wallet-option.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Connector, useConnect } from "wagmi";
+
+export default function WalletOptions() {
+  const { connectors, connect } = useConnect();
+
+  return (
+    <div className="flex gap-x-4">
+      {connectors.map((connector) => (
+        <WalletOption
+          key={connector.uid}
+          connector={connector}
+          onClick={() => connect({ connector })}
+        />
+      ))}
+    </div>
+  );
+}
+
+function WalletOption({ connector, onClick }) {
+  const [ready, setReady] = React.useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      const provider = await connector.getProvider();
+      setReady(!!provider);
+    })();
+  }, [connector]);
+
+  return (
+    <button disabled={!ready} onClick={onClick}>
+      {connector.name}
+    </button>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,48 @@
 import '@/styles/globals.css'
+import {
+  createConfig,
+  http,
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsName,
+  usePublicClient,
+  useSwitchChain,
+  useWalletClient,
+  WagmiProvider,
+} from "wagmi";
+import { sepolia, mainnet, arbitrum, arbitrumSepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  injected,
+  metaMask,
+  coinbaseWallet,
+  safe,
+  walletConnect,
+} from "wagmi/connectors";
+const config = createConfig({
+  chains: [mainnet, sepolia, arbitrum, arbitrumSepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [arbitrum.id]: http(),
+    [arbitrumSepolia.id]: http(),
+  },
+  connectors: [
+    walletConnect({
+      projectId: "5f3d3b2e74be262d11cfdd7d0b9dac10",
+      showQrModal: true,
+    }),
+  ],
+});
 
+const queryClient = new QueryClient();
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
